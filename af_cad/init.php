036bde76f529e75585171f749529ef336b9de642
William Ogier <sir.kitna@gmail.com>
2013-05-28 21:28:54 +1000

    Rewrite plugin with specific xpath query

--- af_cad/init.php
+++ af_cad/init.php
@@ -1,47 +1,41 @@
 <?php
-class Af_CAD extends Plugin {
+class Af_cad extends Plugin {
 
 	private $host;
 
 	function about() {
-		return array(1.1,
-			"Strip unnecessary stuff from all Ctrl+Alt+Del feeds",
-			"Phranx");
+		return array(1.2,
+			"Embed Comic in Ctrl+Alt+Del RSS Feeds", //Feed url http://www.cad-comic.com/rss/rss.xml
+			"antiKk");
 	}
 
 	function init($host) {
 		$this->host = $host;
-
 		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
 	}
-
+	
 	function hook_article_filter($article) {
 		$owner_uid = $article["owner_uid"];
 
 		if (strpos($article["link"], "cad-comic.com") !== FALSE && strpos($article["title"], "News:") === FALSE) {
 			if (strpos($article["plugin_data"], "cad-comic,$owner_uid:") === FALSE) {
-
+		
 				$doc = new DOMDocument();
 				@$doc->loadHTML(fetch_file_contents($article["link"]));
-
+			
 				$basenode = false;
 
 				if ($doc) {
 					$xpath = new DOMXPath($doc);
-					$entries = $xpath->query('(//img[@src])'); // we might also check for img[@class='strip'] I guess...
-
+					$entries = $xpath->query("(//img[contains(@src,'/comics')])");
+					
 					$matches = array();
 
 					foreach ($entries as $entry) {
-
-						if (preg_match("/(http:\/\/v.cdn.cad-comic.com\/comics\/.*)/i", $entry->getAttribute("src"), $matches)) {
-							
-							$basenode = $entry;
-							break;
-						}
+						$basenode = $entry;
 					}
 
-					if ($basenode) {
+				if ($basenode) {
 						$article["content"] = $doc->saveXML($basenode, LIBXML_NOEMPTYTAG);
 						$article["plugin_data"] = "cad-comic,$owner_uid:" . $article["plugin_data"];
 					}
@@ -50,7 +44,6 @@ class Af_CAD extends Plugin {
 				$article["content"] = $article["stored"]["content"];
 			}
 		}
-
 		return $article;
 	}
 	
