ec48419dd110a56cec3d9d572d7b9edafc9ff13e
William Ogier <sir.kitna@gmail.com>
2013-06-13 21:36:20 +1000

    New plugin for Table Titans

--- af_tabletitans/init.php
+++ af_tabletitans/init.php
@@ -1,26 +1,24 @@
 <?php
-class Af_YellowPeril extends Plugin {
+class Af_tabletitans extends Plugin {
 
 	private $host;
 
 	function about() {
-		return array(1.1,
-			"Embed Comic in Yellow Peril RSS Feed", //Feed url http://feeds.feedburner.com/YellowPeril
+		return array(1.0,
+			"Embed Comic in Table Titans RSS Feeds", //Feed url http://www.tabletitans.com/feed
 			"antiKk");
 	}
 
 	function init($host) {
 		$this->host = $host;
-
 		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
 	}
 	
 	function hook_article_filter($article) {
 		$owner_uid = $article["owner_uid"];
-		$original = $article;
 
-		if (strpos($article["link"], "ypcomic.com") !== FALSE && strpos($article["title"], "[Comic]") !== FALSE) {
-			if (strpos($article["plugin_data"], "ypcomic,$owner_uid:") === FALSE) {
+		if (strpos($article["link"], "tabletitans.com") !== FALSE && strpos($article["title"], "Latest Adventure:") !== FALSE) {
+			if (strpos($article["plugin_data"], "tabletitans,$owner_uid:") === FALSE) {
 		
 				$doc = new DOMDocument();
 				@$doc->loadHTML(fetch_file_contents($article["link"]));
@@ -29,7 +27,7 @@ class Af_YellowPeril extends Plugin {
 
 				if ($doc) {
 					$xpath = new DOMXPath($doc);
-					$entries = $xpath->query("(//div[@id='comic'])");
+					$entries = $xpath->query("(//section[@class='comic row'])");
 					
 					$matches = array();
 
@@ -38,9 +36,8 @@ class Af_YellowPeril extends Plugin {
 					}
 
 				if ($basenode) {
-						$article["content"] = $doc->saveXML($basenode, LIBXML_NOEMPTYTAG) . $original["content"];
-						$article["plugin_data"] = "ypcomic,$owner_uid:" . $article["plugin_data"];
-						$article["content"] = $article["content"] . $original["content"];
+						$article["content"] = $doc->saveXML($basenode, LIBXML_NOEMPTYTAG);
+						$article["plugin_data"] = "tabletitans,$owner_uid:" . $article["plugin_data"];
 					}
 				}
 			} else if (isset($article["stored"]["content"])) {
